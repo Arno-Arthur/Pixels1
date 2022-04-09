7b12286781a7b0184a4e51d396b8bb3aaa74c0c6
Arno-Arthur <arturrepin11@gmail.com>
2022-04-09 09:30:46 +0600

    Update 1.1

--- app/src/main/java/com/example/pixels/CollActivity.java
+++ app/src/main/java/com/example/pixels/CollActivity.java
@@ -1,6 +1,10 @@
 package com.example.pixels;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class CollActivity extends SecondActivity{
     @Override
